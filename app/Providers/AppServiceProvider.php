302b40cd90d485374c7e6894e7bf2d119c414f97
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-15 22:31:09 -0300

    Add o Observer no Boot do Laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use App\Models\Transaction;
+use App\Observers\TransactionObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('pt_BR');
+        Transaction::observe(TransactionObserver::class);
     }
 }
