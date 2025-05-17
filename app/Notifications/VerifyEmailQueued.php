5a8d139dcd1d7c312f1122cde181babfa5a6808d
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-17 16:43:51 -0300

    Add o Verify Email e Queue

---
+++ app/Notifications/VerifyEmailQueued.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
+
+class VerifyEmailQueued extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+         return (new MailMessage)
+        ->subject('Verificar e-mail - Carteira Financeira')
+        ->view('emails.custom-verify', [
+            'url' => $verificationUrl,
+            'user' => $notifiable,
+        ]);
+    }
+
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(60),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
+}
