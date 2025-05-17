0159cfe79db068248a0ed5bb563fa38bac129896
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-17 16:45:47 -0300

    Criando Model de Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'target_user_id',
+        'type',
+        'amount',
+        'status',
+        'reverses_id',
+    ];
+
+    protected $casts = [
+        'amount'     => 'float',
+        'created_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function targetUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'target_user_id');
+    }
+
+    public function reversed(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class, 'reverses_id');
+    }
+}
