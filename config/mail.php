8b82712a725c70839cdbc56945ddb3623b108170
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-17 16:44:13 -0300

    Ajustando para o Mail, executar com Queue

--- config/mail.php
+++ config/mail.php
@@ -39,6 +39,7 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
+            'queue' => true,
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
