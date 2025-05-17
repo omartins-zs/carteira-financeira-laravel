cde633a23ee66ae3c5218ae85929b2c7497bb7f5
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-16 23:10:04 -0300

    Forçando a URL gerada com o APP_URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use App\Models\Transaction;
 use App\Observers\TransactionObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Garante que todo URL gerado use esse root
+        URL::forceRootUrl(config('app.url'));
+
+        // Se você estiver usando HTTPS em dev (Valet):
+        // URL::forceScheme('https');
+
         Carbon::setLocale('pt_BR');
         Transaction::observe(TransactionObserver::class);
     }
