a1d49044716f2e05c1e16c82921cf45a3e71813a
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-17 16:44:30 -0300

    Ajustando os Links de Navegação

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,16 +4,22 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                {{-- <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
-                </div>
+                </div> --}}
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                   <x-nav-link :href="route('wallet.index')" :active="request()->routeIs('wallet.index')">
+                        Dashboard
+                    </x-nav-link>
+                    <x-nav-link :href="route('wallet.deposit.form')" :active="request()->routeIs('wallet.deposit.form')">
+                        Depósito
+                    </x-nav-link>
+                    <x-nav-link :href="route('wallet.transfer.form')" :active="request()->routeIs('wallet.transfer.form')">
+                        Transferências
                     </x-nav-link>
                 </div>
             </div>
