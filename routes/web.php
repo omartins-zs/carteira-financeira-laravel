1368f487bfd7f689bfa76e9c69db25f30293f5b0
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-17 16:44:59 -0300

    Ajustando as Rotas Protegidas e da Carteira

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WalletController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TwoFactorCodeController;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +10,22 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('wallet.index');
 })->middleware(['auth', 'verified', 'two.factor'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Rotas protegidas, na mesma ordem de middleware
+Route::middleware(['auth', 'two.factor'])->group(function () {
+
+    Route::middleware(['verified', 'two.factor'])->group(function () {
+        // Carteira
+        Route::get('/wallet', [WalletController::class, 'index'])->name('wallet.index');
+        Route::get('/wallet/deposit', [WalletController::class, 'showDepositForm'])->name('wallet.deposit.form');
+        Route::post('/wallet/deposit', [WalletController::class, 'deposit'])->name('wallet.deposit');
+        Route::get('/wallet/transfer', [WalletController::class, 'showTransferForm'])->name('wallet.transfer.form');
+        Route::post('/wallet/transfer', [WalletController::class, 'transfer'])->name('wallet.transfer');
+        Route::post('/wallet/{transaction}/reverse', [WalletController::class, 'reverse'])->name('wallet.reverse');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
